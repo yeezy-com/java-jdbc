dff9623cc4f700ee0dc9771133283325e6c6dfd8
GiSeok Kang <ime.rokz@gmail.com>
2025-10-16 11:59:28 +0900

    refactor: TransactionTemplate 내 Connection try-with-resources 방식으로 수정

--- jdbc/src/main/java/com/interface21/jdbc/core/TransactionTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/TransactionTemplate.java
@@ -19,32 +19,31 @@ public class TransactionTemplate {
     }
 
     public <T> T execute(Function<Connection, T> executor) {
-        Connection conn = null;
-        T object;
-        try {
-            conn = dataSource.getConnection();
-            conn.setAutoCommit(false);
-
-            object = executor.apply(conn);
-
-            conn.commit();
-        } catch (DataAccessException | SQLException e) {
-            log.error(e.getMessage(), e);
+        try (final var conn = dataSource.getConnection()) {
             try {
-                conn.rollback();
-            } catch (SQLException sqlException) {
-                throw new DataAccessException(sqlException);
-            }
-            throw new DataAccessException(e);
-        } finally {
-            try {
-                conn.setAutoCommit(true);
-                conn.close();
-            } catch (SQLException e) {
+                conn.setAutoCommit(false);
+
+                T object = executor.apply(conn);
+
+                conn.commit();
+                return object;
+            } catch (DataAccessException | SQLException e) {
+                log.error(e.getMessage(), e);
+                try {
+                    conn.rollback();
+                } catch (SQLException sqlException) {
+                    throw new DataAccessException(sqlException);
+                }
                 throw new DataAccessException(e);
+            } finally {
+                try {
+                    conn.setAutoCommit(true);
+                } catch (SQLException e) {
+                    throw new DataAccessException(e);
+                }
             }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
         }
-
-        return object;
     }
 }
