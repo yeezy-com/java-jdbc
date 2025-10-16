b2390e653fb58b514f25a3d43bda47247984ab35
GiSeok Kang <ime.rokz@gmail.com>
2025-10-16 11:33:55 +0900

    refactor: changePassword 트랜잭션 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,19 +1,27 @@
 package com.techcourse.service;
 
+import com.interface21.jdbc.core.TransactionTemplate;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
 import java.util.NoSuchElementException;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserService {
 
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+
+    private final TransactionTemplate transactionTemplate;
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final DataSource dataSource, final UserDao userDao, final UserHistoryDao userHistoryDao) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.transactionTemplate = new TransactionTemplate(dataSource);
     }
 
     public User findById(final long id) {
@@ -26,10 +34,14 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
+        transactionTemplate.execute(connection -> {
+            final var user = findById(id);
+            log.debug("user password change: {}", user.getId());
+            user.changePassword(newPassword);
 
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+            return null;
+        });
     }
 }
